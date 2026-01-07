55c54e4558e3251f0f551788c1a48307c19b5ec9
josefranciscocruzcorro <josefrnaciscocruzcorro@gmail.com>
2026-01-07 12:37:38 -0500

    sa

--- init-multiple-dbs.sh
+++ init-multiple-dbs.sh
@@ -3,17 +3,17 @@ set -e
 set -u
 
 function create_user_and_database() {
-	local database=$1
-	echo "  Creating user and database '$database'"
-	psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-	    CREATE DATABASE $database;
-	    GRANT ALL PRIVILEGES ON DATABASE $database TO $POSTGRES_USER;
+local database=$1
+echo "  Creating user and database '$database'"
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+    CREATE DATABASE $database;
+    GRANT ALL PRIVILEGES ON DATABASE $database TO $POSTGRES_USER;
 EOSQL
 }
 
 if [ -n "$POSTGRES_DB" ]; then
-	echo "Multiple database creation requested"
-	create_user_and_database db_clientes
-	create_user_and_database db_cuentas
-	echo "Multiple databases created"
+echo "Multiple database creation requested"
+create_user_and_database db_clientes
+create_user_and_database db_cuentas
+echo "Multiple databases created"
 fi
